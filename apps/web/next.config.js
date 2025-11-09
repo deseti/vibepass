550a26271534f9b30b825eca6e73a3beba08ef45
deseti <almachzumisetiaji1@gmail.com>
2025-11-08 21:53:57 -0800

    Suppress MetaMask SDK webpack warnings

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,8 +9,25 @@ const nextConfig = {
     NEXT_PUBLIC_CHAIN_ID: process.env.NEXT_PUBLIC_CHAIN_ID || '8453',
   },
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.fallback = { 
+      fs: false, 
+      net: false, 
+      tls: false,
+      crypto: false,
+    };
+    config.externals.push(
+      'pino-pretty', 
+      'lokijs', 
+      'encoding',
+      '@react-native-async-storage/async-storage'
+    );
+    
+    // Ignore warnings for optional dependencies
+    config.ignoreWarnings = [
+      { module: /@metamask\/sdk/ },
+      { module: /@react-native-async-storage/ },
+    ];
+    
     return config;
   },
 }
